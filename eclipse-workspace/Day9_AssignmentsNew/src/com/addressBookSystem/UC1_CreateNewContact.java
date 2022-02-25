6f00184ad813ee889607c8ef3de24f5c0d38f4a3
Priya Gadhave <priyagadhave20@gmail.com>
2022-02-25 22:09:48 +0530

    First commit

---
+++ eclipse-workspace/Day9_AssignmentsNew/src/com/addressBookSystem/UC1_CreateNewContact.java
@@ -0,0 +1,96 @@
+package com.addressBookSystem;
+
+
+class Contacts {
+    //variables
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private long phoneNumber;
+    private int zip;
+    
+    public Contacts(String firstName, String lastName, String address, String city, String state, long phoneNumber, int zip) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.zip = zip;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return  "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", zip=" + zip;
+    }
+}
+public class UC1_CreateNewContact {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		
+	}
+}
